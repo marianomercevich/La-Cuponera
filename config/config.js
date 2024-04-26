e440c4a4c5c37fcf2c2725068752c89e1c7f0706
Mariano Mercevich <marianomercevich@gmail.com>
2024-04-26 01:11:42 -0300

    fix structure usuarios branch

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-/*import { config } from "dotenv"; config();
+import { config } from "dotenv"; config();
 export const PORT = process.env.PORT || 8080;
 export const MONGO_URI = process.env.MONGO_URI;
 export const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
@@ -8,7 +8,7 @@ export const JWT_CLIENT_ID = process.env.JWT_CLIENT_ID;
 export const JWT_CLIENT_SECRET = process.env.JWT_CLIENT_SECRET;
 export const JWT_COMPANY_SECRET = process.env.JWT_COMPANY_SECRET;
 export const JWT_EMPRESA_SECRET = process.env.JJWT_EMPRESA_SECRET;
-export const ENVIRONMENT = process.env.ENVIRONMENT; 
+export const ENVIRONMENT = process.env.ENVIRONMENT
 
 /* 
 export const SIGNED_COOKIE_KEY = process.env.SIGNED_COOKIE_KEY;
@@ -16,4 +16,5 @@ export const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
 export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 export const PERSISTENCE = process.env.PERSISTENCE;
 export const NODEMAILER_USER = process.env.NODEMAILER_USER;
-export const NODEMAILER_PASS = process.env.NODEMAILER_PASS;
+export const NODEMAILER_PASS = process.env.NODEMAILER_PASS
+*/
