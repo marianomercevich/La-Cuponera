f3059fb8fee5d441b52adad36fac605f10a95753
Mariano Mercevich <marianomercevich@gmail.com>
2024-04-27 02:25:29 -0300

    fix cupones branch structure

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,14 @@
-/*import { config } from "dotenv"; config();
+import { config } from "dotenv"; config();
 export const PORT = process.env.PORT || 8080;
 export const MONGO_URI = process.env.MONGO_URI;
-export const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+export const MONGO_DB_NAME_PROD = process.env.MONGO_DB_NAME_PROD;
+export const MONGO_DB_NAME_TEST = process.env.MONGO_DB_NAME_TEST;
 export const SECRET_PASS = process.env.SECRET_PASS;
 export const PRIVATE_KEY = process.env.PRIVATE_KEY;
 export const JWT_CLIENT_ID = process.env.JWT_CLIENT_ID;
 export const JWT_CLIENT_SECRET = process.env.JWT_CLIENT_SECRET;
-export const JWT_COMPANY_SECRET = process.env.JWT_COMPANY_SECRET;
-export const JWT_EMPRESA_SECRET = process.env.JJWT_EMPRESA_SECRET;
-export const ENVIRONMENT = process.env.ENVIRONMENT; 
+export const ENVIRONMENT = process.env.ENVIRONMENT
+
 
 /* 
 export const SIGNED_COOKIE_KEY = process.env.SIGNED_COOKIE_KEY;
