48f78e85330e8b90a16cf5dc9ac3a017518219d9
Mariano Mercevich <marianomercevich@gmail.com>
2024-05-03 13:07:56 -0300

    fix bug

--- src/controllers/CuponsController.js
+++ src/controllers/CuponsController.js
@@ -1,44 +1,39 @@
 import Coupon from '../models/coupon.js';
-import multer from 'multer';
-
-// Configuración de multer para la subida de imágenes
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/public/img'); // Directorio donde se guardarán las imágenes
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname); // Nombre del archivo guardado
-  }
-});
-
-const upload = multer({ storage: storage });
+import upload from '../middlewares/upload.js';
 
 // Función para manejar la creación de cupones
 export const createCoupon = async (req, res) => {
     try {
-        const { title, description, discount, expirationDate, createdBy } = req.body;
+        // Utiliza el middleware de Multer para manejar la subida de archivos
+        upload.single('imagen')(req, res, async function (err) {
+            if (err) {
+                return res.status(400).json({ message: 'Error al subir la imagen' });
+            }
 
-        // Verificar si se proporcionó una imagen en la solicitud
-        let imagePath = '';
-        if (req.file) {
-            imagePath = req.file.path; // Ruta de la imagen guardada
-        }
+            const { title, description, discount, expirationDate, createdBy } = req.body;
 
-        // Crear un nuevo cupón
-        const newCoupon = new Coupon({
-            title,
-            description,
-            discount,
-            expirationDate,
-            createdBy,
-            image: imagePath // Guardar la ruta de la imagen en el documento del cupón
+            // Verificar si se proporcionó una imagen en la solicitud
+            let imagePath = '';
+            if (req.file) {
+                imagePath = req.file.filename; // Nombre del archivo guardado
+            }
+
+            // Crear un nuevo cupón
+            const newCoupon = new Coupon({
+                title,
+                description,
+                discount,
+                expirationDate,
+                createdBy,
+                imagePath // Guardar el nombre del archivo en el documento del cupón
+            });
+
+            // Guardar el nuevo cupón en la base de datos
+            await newCoupon.save();
+
+            // Devolver el cupón creado como respuesta
+            res.status(201).json(newCoupon);
         });
-
-        // Guardar el nuevo cupón en la base de datos
-        await newCoupon.save();
-
-        // Devolver el cupón creado como respuesta
-        res.status(201).json(newCoupon);
     } catch (error) {
         console.error('Error en createCoupon:', error);
         res.status(500).json({ message: 'Error interno del servidor' });
@@ -48,29 +43,36 @@ export const createCoupon = async (req, res) => {
 // Función para manejar la actualización de cupones
 export const updateCoupon = async (req, res) => {
     try {
-        const { title, description, discount, expirationDate } = req.body;
+        // Utiliza el middleware de Multer para manejar la subida de archivos
+        upload.single('imagen')(req, res, async function (err) {
+            if (err) {
+                return res.status(400).json({ message: 'Error al subir la imagen' });
+            }
 
-        // Verificar si se proporcionó una imagen en la solicitud
-        let imagePath = '';
-        if (req.file) {
-            imagePath = req.file.path; // Ruta de la imagen guardada
-        }
+            const { title, description, discount, expirationDate } = req.body;
 
-        // Actualizar el cupón existente
-        const updatedCoupon = await Coupon.findByIdAndUpdate(req.params.id, {
-            title,
-            description,
-            discount,
-            expirationDate,
-            image: imagePath // Guardar la ruta de la nueva imagen en el documento del cupón
-        }, { new: true });
+            // Verificar si se proporcionó una imagen en la solicitud
+            let imagePath = '';
+            if (req.file) {
+                imagePath = req.file.filename; // Nombre del archivo guardado
+            }
 
-        if (!updatedCoupon) {
-            return res.status(404).send('Cupón no encontrado');
-        }
+            // Actualizar el cupón existente
+            const updatedCoupon = await Coupon.findByIdAndUpdate(req.params.id, {
+                title,
+                description,
+                discount,
+                expirationDate,
+                imagePath // Guardar la ruta de la nueva imagen en el documento del cupón
+            }, { new: true });
 
-        // Devolver el cupón actualizado como respuesta
-        res.json(updatedCoupon);
+            if (!updatedCoupon) {
+                return res.status(404).send('Cupón no encontrado');
+            }
+
+            // Devolver el cupón actualizado como respuesta
+            res.json(updatedCoupon);
+        });
     } catch (error) {
         console.error('Error en updateCoupon:', error);
         res.status(500).json({ message: 'Error interno del servidor' });
