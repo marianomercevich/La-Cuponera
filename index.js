4bf697a73829609a291d66acadec8f07b9d195f2
Mariano Mercevich <marianomercevich@gmail.com>
2024-05-13 15:40:53 -0300

    fix swagger bug

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import YAML from 'yamljs';
 // Configuración de Express
 const app = express();
 const PORT = process.env.PORT || 5000;
-const swaggerDocument = YAML.load('./src/doc/Vendedores.yaml'); // Ruta a tu archivo de especificación Swagger
+const swaggerDocument = YAML.load('src/doc/Vendedores.yaml'); // Ruta a tu archivo de especificación Swagger
 
 // Middleware
 app.use(bodyParser.json());
