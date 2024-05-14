2bc4503a7eb095840ffffe110c7ef49389f75e32
Mariano Mercevich <marianomercevich@gmail.com>
2024-05-14 16:59:08 -0300

    fix

--- vercel.sh
+++ vercel.sh
@@ -2,12 +2,12 @@
 
 source .env
 
-if [[ $BRANCH_NAME == "Cupones"  ]] ; then 
-    npm run build:cuponnes
-elif [[ $BRANCH_NAME == "Usuarios"  ]] ; then
+if [[ $BRANCH_NAME == "Usuarios"  ]] ; then
     npm run build:usuarios
-elif [[ $BRANCH_NAME == "Vendedores"  ]] ; then
-    npm run build:vendedores
+elif [[ $BRANCH_NAME == "Cupones"  ]] ; then 
+    npm run build:cuponnes
+elif [[ $BRANCH_NAME == "Empresas"  ]] ; then
+    npm run build:empresas
 else
     echo $BRANCH_NAME
     echo "BRANCH NOT MATCHED"
