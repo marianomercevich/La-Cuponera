806d30eb32b945ce7ac3bfcb4a03f9152398d6e5
Mariano Mercevich <marianomercevich@gmail.com>
2024-05-22 12:55:39 -0300

    fix users

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ import YAML from 'yamljs';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const swaggerDocument = YAML.load('./doc/Cuponeros.yaml'); // Ruta a tu archivo de especificación Swagger
+const swaggerDocument = YAML.load('./src/doc/Cuponeros.yaml'); // Ruta a tu archivo de especificación Swagger
 
 // Middleware
 app.use(bodyParser.json());
--- src/models/user.js
+++ src/models/user.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose';
 
 // Definir el esquema del usuario
 const userSchema = new mongoose.Schema({
-  id: {type: Number, required: true},
+  id: {type: Number, required: false},
   nombre: { type: String, required: true },
   apellido: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   contraseña: { type: String, required: true },
-  registroFecha: {type: String, required: true },
-  estadoVerificacion: { type: String, required: true, enum: ['Pendiente', 'Aprobada', 'Desaprobada'] }, 
+  registroFecha: {type: String, required: false },
+  estadoVerificacion: { type: String, required: false, enum: ['Pendiente', 'Aprobada', 'Desaprobada'] }, 
 });
 
 // Exportar el modelo de usuario basado en el esquema
