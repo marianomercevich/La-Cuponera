d5a4425b37278045d3a7477bd907f010c122f7a6
Mariano Mercevich <marianomercevich@gmail.com>
2024-06-21 18:50:20 -0300

    fix

--- src/controllers/vendedorAuthController.js
+++ src/controllers/vendedorAuthController.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import Vendedor from '../models/Vendedores.js';
 import { enviarCorreoRegistro } from '../nodemailer/mailer.js';
--- src/routes/vendedoresRoutes.js
+++ src/routes/vendedoresRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import { loginVendedor, registerVendedor } from '../controllers/vendedorAuthController.js';
 import Vendedor from '../models/Vendedores.js';
 
