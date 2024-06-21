69cec3a2ae71ddb437bd3bf336a036ee26d7252e
Mariano Mercevich <marianomercevich@gmail.com>
2024-06-21 16:10:27 -0300

    fix

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ import { conexion_App  } from './src/config/database.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
