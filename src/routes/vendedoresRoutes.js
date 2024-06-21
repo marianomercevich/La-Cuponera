e3c5a3d0d5ae92e81f3e83482b136f5969ad6e22
Mariano Mercevich <marianomercevich@gmail.com>
2024-06-21 18:25:01 -0300

    fix bycrypt

--- src/routes/vendedoresRoutes.js
+++ src/routes/vendedoresRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { loginVendedor, registerVendedor } from '../controllers/vendedorAuthController.js';
 import Vendedor from '../models/Vendedores.js';
 
