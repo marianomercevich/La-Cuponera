14fcf7fd6d262c3bf4f6ede859296557f5ad8257
Mariano Mercevich <marianomercevich@gmail.com>
2024-06-23 04:43:12 -0300

    fix

--- src/models/user.js
+++ src/models/user.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 // Definir el esquema del usuario
 const userSchema = new mongoose.Schema({
   id: {type: Number, required: false},
-  nombre: { type: String, required: false },
-  apellido: { type: String, required: false },
+  nombre: { type: String, required: true },
+  apellido: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   registroFecha: {type: String, required: false },
